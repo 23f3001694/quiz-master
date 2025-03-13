90c54ffe2ec76f477b583aaacf73914eaab611b1
23f3001694 <23f3001694@ds.study.iitm.ac.in>
2025-03-13 17:53:01 +0530

    Fix references

--- utils/setup_test_db.py
+++ utils/setup_test_db.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import create_app
 from app.models.database import db, Admin, User, Subject, Chapter, Quiz, Question, Score
 from datetime import datetime, time, timedelta
